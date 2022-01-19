1d4314dd69adfc785e1468c4c3cc343c752f2a59
rajasyah <rajasyahacp20@gmail.com>
2022-01-19 09:12:57 +0700

    Create relation database

--- app/product/controller.js
+++ app/product/controller.js
@@ -2,10 +2,34 @@ const path = require("path");
 const fs = require("fs");
 const config = require("../config");
 const Product = require("./model");
+const Category = require("../category/model");
+const Tag = require("../tag/model");
 
 const store = async (req, res, next) => {
   try {
     let payload = req.body;
+
+    if (payload.category) {
+      let category = await Category.findOne({
+        name: { $regex: payload.category, $options: "i" },
+      });
+      if (category) {
+        payload = { ...payload, category: category._id };
+      } else {
+        delete payload.category;
+      }
+    }
+    if (payload.tags && payload.tags.length > 0) {
+      let tags = await Tag.find({
+        name: { $in: payload.tags },
+      });
+      if (tags.length) {
+        payload = { ...payload, tags: tags.map((tag) => tag._id) };
+      } else {
+        delete payload.tags;
+      }
+    }
+
     if (req.file) {
       let tmp_path = req.file.path;
       let originalExt =
@@ -63,6 +87,28 @@ const update = async (req, res, next) => {
   try {
     let payload = req.body;
     let { id } = req.params;
+
+    if (payload.category) {
+      let category = await Category.findOne({
+        name: { $regex: payload.category, $options: "i" },
+      });
+      if (category) {
+        payload = { ...payload, category: category._id };
+      } else {
+        delete payload.category;
+      }
+    }
+    if (payload.tags && payload.tags.length > 0) {
+      let tags = await Tag.find({
+        name: { $in: payload.tags },
+      });
+      if (tags.length) {
+        payload = { ...payload, tags: tags.map((tag) => tag._id) };
+      } else {
+        delete payload.tags;
+      }
+    }
+
     if (req.file) {
       let tmp_path = req.file.path;
       let originalExt =
@@ -131,7 +177,9 @@ const index = async (req, res, next) => {
     let { skip = 0, limit = 10 } = req.query;
     let product = await Product.find()
       .skip(parseInt(skip))
-      .limit(parseInt(limit));
+      .limit(parseInt(limit))
+      .populate("category")
+      .populate("tags");
     return res.json(product);
   } catch (err) {
     next(err);
